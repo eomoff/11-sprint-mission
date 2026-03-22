974ffbf80f80f94bc9d4f505daa9313a92c726a0
eomoff <eomoff@gmail.com>
2026-03-22 21:56:10 +0900

    Bean 선언 및 테스트

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,13 +1,47 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiscodeitApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DiscodeitApplication.class);
+
+	// 테스트용 유저 생성
+	static User setupUser(UserService userService) {
+		return userService.create("woody");
+	}
+
+	// 테스트용 채널 생성
+	static Channel setupChannel(ChannelService channelService) {
+		return channelService.create("공지");
+	}
+
+	// 메세지 생성 테스트
+	static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+		Message message = messageService.create("안녕하세요", channel.getId(), author.getId());
+		logger.info("메세지 생성: {}", message.getId());
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+
+		UserService userService = context.getBean(UserService.class);
+		ChannelService channelService = context.getBean(ChannelService.class);
+		MessageService messageService = context.getBean(MessageService.class);
+
+		User user = setupUser(userService);
+		Channel channel = setupChannel(channelService);
+		messageCreateTest(messageService, channel, user);
 	}
 
 }
