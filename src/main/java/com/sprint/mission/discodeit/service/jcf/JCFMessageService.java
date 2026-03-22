2d0d944e52add20f90f5569822a74ac751a247e4
eomoff <eomoff@gmail.com>
2026-03-22 20:26:23 +0900

    Basic 코드 추가에 따른 jcf 수정

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,20 +1,33 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class JCFMessageService implements MessageService {
     private final Map<UUID, Message> data;
+    private final UserService userService;
+    private final ChannelService channelService;
 
-    public JCFMessageService() {
+    public JCFMessageService(UserService userService, ChannelService channelService) {
         this.data = new HashMap<>();
+        this.userService = userService;
+        this.channelService = channelService;
     }
-
     @Override
-    public Message create(String content) {
-        Message message = new Message(content);
+    public Message create(String content, UUID channelId, UUID authorId) {
+        if (userService.find(authorId) == null) {
+            throw new IllegalArgumentException("존재하지 않는 사용자입니다: " + authorId);
+        }
+        if (channelService.find(channelId) == null) {
+            throw new IllegalArgumentException("존재하지 않는 채널입니다: " + channelId);
+        }
+
+        Message message = new Message(content, channelId, authorId);
         data.put(message.getId(), message);
         return message;
     }
