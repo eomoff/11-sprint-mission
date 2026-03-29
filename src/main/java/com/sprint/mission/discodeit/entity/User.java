5665a006a06d7941bc8f2a2e17f8a7b656f0506c
eomoff <eomoff@gmail.com>
2026-03-29 16:33:03 +0900

    entity Channel Type 추가 및 Null 방지 보완

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -3,31 +3,38 @@ package com.sprint.mission.discodeit.entity;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
 public class User implements Serializable{
+    // 직렬화
     private static final long serialVersionUID = 1L;
     // 공통 필드
     private UUID id;
-    private Long createdAt; // 언제 만들어졌는지 파악
-    private Long updatedAt; // 언제 마지막으로 수정됐는지 파악
+    private Instant createdAt; // 언제 만들어졌는지 파악
+    private Instant updatedAt; // 언제 마지막으로 수정됐는지 파악
 
     // User 필드
     private String name;
 
     // 생성자
     public User(String name) {
-        long now = System.currentTimeMillis();
         this.id = UUID.randomUUID();
-        this.createdAt = now;
-        this.updatedAt = now;
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
         this.name = name;
     }
 
     // update
-    public void update(String name) {
-        this.name = name;
-        this.updatedAt = System.currentTimeMillis();
+    public void update(String newName) {
+        boolean anyValueUpdated = false;
+        if (newName != null && !newName.equals(this.name)) {
+            this.name = newName;
+            anyValueUpdated = true;
+        }
+        if (anyValueUpdated) {
+            this.updatedAt = Instant.now();
+        }
     }
 }
