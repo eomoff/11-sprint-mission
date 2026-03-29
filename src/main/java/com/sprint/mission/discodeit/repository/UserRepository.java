e473532df0e1f06682eedb8cb7368185373575aa
eomoff <eomoff@gmail.com>
2026-03-29 19:37:20 +0900

    인터페이스 추가 및 수정

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,15 +1,18 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository {
     User save(User user);
-
-    User findById(UUID id);
-
+    Optional<User> findById(UUID id);
+    Optional<User> findByUsername(String userName);
     List<User> findAll();
-
-    void delete(UUID id);
+    boolean existsById(UUID id);
+    void deleteById(UUID id);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String userName);
 }
